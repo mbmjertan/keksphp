59527ffd36fdda6b61474800693c1c7781b4506f
Mario Borna Mjertan <mbm@educateam.hr>
2021-05-03 14:33:34 +0200

    Update SettingKeksSeller.php

--- examples/SettingKeksSeller.php
+++ examples/SettingKeksSeller.php
@@ -1,9 +1,9 @@
 <?php
 include __DIR__.'/../vendor/autoload.php';
 $KeksSeller = new \KeksPHP\KeksSeller;
-$KeksSeller->cid = "C00925";
-$KeksSeller->tid = "P00952";
-$KeksSeller->setHashKey("6D55AD0F8A7CA30F1787C518");
+$KeksSeller->cid = "A12345";
+$KeksSeller->tid = "P12345";
+$KeksSeller->setHashKey("KFKFAKAKLOA12");
 $KeksSeller->successRedirect = 'https://google.com';
 $KeksSeller->failRedirect = 'https://bing.com';
 $KeksSeller->provideValidateAdviceFunction(function($KeksTransaction){
